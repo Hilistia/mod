d4639b8df90a8399e95766a4c8eea4547146ecf6
Martin M Sheeks <martin.sheeks@gmail.com>
2024-12-27 21:28:49 -0600

    init mod and add cardinal components dependency

---
+++ src/client/java/com/hilistia/hilistiaMod/client/HilistiaModClient.java
@@ -0,0 +1,10 @@
+package com.hilistia.hilistiaMod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HilistiaModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/hilistia/hilistiaMod/HilistiaMod.java
@@ -0,0 +1,10 @@
+package com.hilistia.hilistiaMod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HilistiaMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
